2241ce080c341f159ec0a5d7be87f8e05720d856
lg0017 <luizmb16@gmail.com>
2020-03-13 15:55:22 -0300

    Corrigindo erros

--- src/main.c
+++ src/main.c
@@ -27,15 +27,11 @@ char pop() {
   return c;
 }
 
-#include <stdio.h>
-const int tam_buffer=100;
-
 int main() {
   char a = '0';
   int cont = 0;
   char buffer[tam_buffer];
   fgets(buffer, tam_buffer, stdin);
-  printf("%s", buffer);
 
   /* Tratando a entrada */
   while(a != '\n'){
